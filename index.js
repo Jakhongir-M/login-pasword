af5c9e5bb0b6791f12857963c3de30ef166ff7fd
Jakhongir Mirzakhmatov <94097414+Jakhongir-M@users.noreply.github.com>
2022-05-20 05:38:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+let login = prompt("Login kiriting!!!");
+if (login == "admin") {
+} else {
+  alert("siz notogri login kiritingiz");
+}
+let password = +prompt("parol kiriting");
+if (password === 12345) {
+  alert("Xush kelibsiz");
+} else {
+  alert("!!!Parolni qaytadan tering");
+}
+
+// let login = admin;
+// admin =
+// let password = 12345;
+// if (prompt("login kiriting") == login) {
+//   alert("parol kiriting");
+// } else if (password === prompt("parol kiriting")) {
+//   alert("parolni qaytadan kiriting");
+// } else {
+//   alert("kirish man etilgan");
+// }
